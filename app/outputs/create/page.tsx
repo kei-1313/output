61b05b08497f80c5242ab7a64057999555cd16a0
kei-1313 <candydog666@gmail.com>
2024-04-06 18:01:54 +0900

    1 以前追加したタグを追加できるように作成 2 タグモーダルの作成

--- app/outputs/create/page.tsx
+++ app/outputs/create/page.tsx
@@ -18,61 +18,89 @@ const outputsCreatePage = () => {
   const [source, setSource] = useState('')
   const [tags, setTags] = useState<Tag[]>([])
   const [tagText, setTagText] = useState('')
+  const [isTagModalShow, setIsTagModalShow] = useState(false)
 
+  //タグモーダルの開閉
+  const handleShowTagModal = () => {
+    setIsTagModalShow(!isTagModalShow)
+  }
+
+  //入力値をタグにする
   const handleEnterTag = (event: React.KeyboardEvent<HTMLInputElement>) => {
-    //Enterが押された場合
-    console.log(event.key);
-    
-    if(event.key === 'Enter') {
-      if(tagText === '') {
-        event.preventDefault();
-        return
-      }
-
+    //入力値があり、Enterが押された場合
+    if (event.key === 'Enter' && tagText.trim() !== '') {
       event.preventDefault();
       const addId = tags.length + 1
       const newTag = {
         id: addId,
-        text: tagText
+        text: tagText.trim()
       }
 
       setTags(prevTags => [...prevTags, newTag])
       setTagText('')
     }
   }
-  
+
+  //入力したタグを削除する
   const handleDeleteTag = (id: number) => {
     setTags(prevTags => prevTags.filter(tag => tag.id !== id));
+  }
+
+  //以前追加したタグを追加する
+  const handleClickLastTag = (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault()
+    const addId = tags.length + 1
+    const newTag = {
+      id: addId,
+      text: event.currentTarget.innerHTML.trim()
+    }
+
+    setTags(prevTags => [...prevTags, newTag])
   }
 
 	return (
     <form className="min-h-screen">
       <div className="flex h-14 items-center justify-between gap-2 px-4">
-        <div>
+        <div className="lg:flex-1">
           <button className="rounded-full w-[36px] h-[36px] hover:bg-sky-50 transition duration-300">
             <FaArrowLeft width={20} height={20} className="mx-auto text-gray-400"/>
           </button>
         </div>
-        <div>
-          <div>
+        <div className="flex-1 lg:justify-center">
+          <div className="text-center">
             <button type="submit" className="w-20 text-center text-white text-sm rounded-full bg-cyan-950 py-1">保存</button>
           </div>
         </div>
-        <div className="flex gap-2 pr-40 relative">
-          <div className="cursor-pointer">
+        <div className="flex items-center gap-2 relative flex-1 justify-center">
+          <div onClick={handleShowTagModal} className="cursor-pointer hover:bg-slate-300/50 transition duration-300 px-3 py-1 rounded-full">
+          {isTagModalShow? (
+            <span className="text-sm text-slate-500">閉じる</span>
+          ):(
             <span className="text-sm text-slate-500">#タグ</span>
+          )}
           </div>
-          <div className="absolute top-8 -left-[100px] w-72 border border-slate-300/50 rounded-t-xl text-sm leading-normal">
-           <div className="flex flex-wrap gap-2 p-3">
-              {tags?.map((tag, index) => (
-                <div className="flex gap-1 items-center border border-slate-300/50  rounded-full py-1 px-3" key={index}>
-                  <span>{tag.text}</span>
-                  <span onClick={() => handleDeleteTag(tag.id)} className="text-slate-300 cursor-pointer hover:text-slate-400 transition duration-300">✗</span>
-                </div>
-              ))}
-              <input className="outline-none" value={tagText} onChange={(event) => setTagText(event.target.value)} onKeyUp={handleEnterTag} type="text" name="" id="" placeholder="タグを追加"/>
-           </div>
-          </div>
+          {isTagModalShow? (
+            <div className="absolute top-9 right-0 w-72 border border-slate-300/50 rounded-t-xl text-sm leading-normal">
+            <div className="flex flex-wrap gap-2 p-3">
+                {tags?.map((tag, index) => (
+                  <div className="flex gap-1 items-center border border-slate-300/50  rounded-full py-1 px-3" key={index}>
+                    <span>{tag.text}</span>
+                    <span onClick={() => handleDeleteTag(tag.id)} className="text-slate-300 cursor-pointer hover:text-slate-400 transition duration-300">✗</span>
+                  </div>
+                ))}
+                <input className="outline-none" value={tagText} onChange={(event) => setTagText(event.target.value)} onKeyPress={handleEnterTag} type="text" name="" id="" placeholder="タグを追加"/>
+            </div>
+            <div>
+              <div className="p-1.5 grid grid-cols-3 gap-2">
+                <button onClick={handleClickLastTag} className="p-2.5 text-sm text-slate-600 block bg-slate-300/50 rounded-lg cursor-pointer">React</button>
+                <button onClick={handleClickLastTag} className="p-2.5 text-sm text-slate-600 block bg-slate-300/50 rounded-lg cursor-pointer">React</button>
+                <button onClick={handleClickLastTag} className="p-2.5 text-sm text-slate-600 block bg-slate-300/50 rounded-lg cursor-pointer">React</button>
+              </div>
+            </div>
+            </div>
+          ): (
+            <></>
+          )}
         </div>
       </div>
       <div className="max-w-[580px] mx-auto px-6 py-24">
@@ -123,7 +151,7 @@ const outputsCreatePage = () => {
         
       </div>
       {/* create page footer */}
-      <div className="flex justify-between px-8">
+      <div className="flex justify-between px-8 fixed bottom-10 left-0 w-full">
           <div>
 
           </div>
