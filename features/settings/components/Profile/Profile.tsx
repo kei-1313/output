dc8bb68e8620927fbf06c576075903490e465aea
kei-1313 <candydog666@gmail.com>
2024-04-06 19:28:01 +0900

    settingsページの作成 Profileコンポーネントの作成

---
+++ features/settings/components/Profile/Profile.tsx
@@ -0,0 +1,12 @@
+import ProfileImage from "./ProfileImage"
+
+const Profile = () => { 
+	return (
+		<div className="mt-10">
+      <ProfileImage></ProfileImage>
+    </div>
+	)
+}
+
+export default Profile
+
---
+++ features/settings/components/Profile/ProfileImage.tsx
@@ -0,0 +1,19 @@
+import Image from "next/image"
+
+const ProfileImage = () => { 
+	return (
+		<div className="overflow-hidden rounded-2xl border border-slate-300/50 p-6 md:p-7">
+      <div className="flex gap-6 items-center">
+        <div>
+          <Image className="rounded-full" src="/images/dammy.png" width={64} height={64} alt="dammy"/>
+        </div>
+        <div>
+          <button className="inline-flex h-10 items-center gap-2 text-sm rounded-full border border-slate-500/50 px-4 hover:bg-slate-100 transition duration-300">プロフィール画像を変更</button>
+        </div>
+      </div>
+    </div>
+	)
+}
+
+export default ProfileImage
+
