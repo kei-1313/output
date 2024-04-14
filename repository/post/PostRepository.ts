ee5f00aed8930cb058bbd88b0e1ccb244340bc58
kei-1313 <candydog666@gmail.com>
2024-04-14 12:24:40 +0900

    現在の日時から投稿時間の差分を表示

--- repository/post/PostRepository.ts
+++ repository/post/PostRepository.ts
@@ -3,7 +3,17 @@ import prisma from "@/lib/db"
 export const createPostRepository = () => {
   return {
     findPostAll: async () => {
-      return await prisma.post.findMany()
+      return await prisma.post.findMany(
+        {
+          include: {
+            User:true,
+            PostFormatBases:true,
+            PostFormatChallenges:true,
+            CategoryRelations: true,
+            Likes:true,
+          }
+        }
+      )
     }
   }
 }
