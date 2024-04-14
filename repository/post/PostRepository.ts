9cf29ecfc81c50409275429d127ee38f36a60bb1
kei-1313 <candydog666@gmail.com>
2024-04-14 12:26:40 +0900

    必要ないテーブルを削除

--- repository/post/PostRepository.ts
+++ repository/post/PostRepository.ts
@@ -7,8 +7,6 @@ export const createPostRepository = () => {
         {
           include: {
             User:true,
-            PostFormatBases:true,
-            PostFormatChallenges:true,
             CategoryRelations: true,
             Likes:true,
           }
